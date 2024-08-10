b0174c5f0e36406fe6bf0997af0c548c9f2a7f1c
sceredi <ceredi.simone.iti@gmail.com>
2024-08-10 10:39:43 +0200

    chore: configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,3 @@
-/* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
@@ -6,6 +5,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
