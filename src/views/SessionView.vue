68ca36ecd6c9f1f52fe47479fe415292db7c3f3c
Luca Fabri <luca.fabri1999@gmail.com>
2024-08-12 10:27:51 +0200

    feat: scrolling and responsive chat

--- src/views/SessionView.vue
+++ src/views/SessionView.vue
@@ -1,16 +1,15 @@
 <script setup>
-import Chat from '@/components/session/SessionChat.vue'
-import SessionFrame from '../components/session/SessionFrame.vue'
+import SessionChat from '@/components/session/SessionChat.vue'
+import SessionFrame from '@/components/session/SessionFrame.vue'
 </script>
 
 <template>
-  <div class="row py-5">
-    <div class="col-md-8 col-12 py-3">
+  <div class="row h-100 py-5">
+    <div class="col-md-8 col-12 py-5">
       <SessionFrame />
     </div>
-    <div class="col-md-4 col-12 d-flex flex-column py-3">
-      <div class="content flex-grow-1" />
-      <Chat />
+    <div class="col-md-4 col-12 d-flex align-items-end py-5">
+      <SessionChat/>
     </div>
   </div>
 </template>
