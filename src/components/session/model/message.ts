e879ae4f70d0747d124bf7b4ed1fc25a535d2b53
Luca Fabri <luca.fabri1999@gmail.com>
2024-08-27 15:48:38 +0200

    fix: add shared model

---
+++ src/components/session/model/message.ts
@@ -0,0 +1,70 @@
+import { User } from './user'
+
+export enum Notification {
+  JOINROOM,
+  LEAVEROOM
+}
+
+export type MessageContent = Notification | string
+
+export interface Message<X extends MessageContent> {
+  readonly content: X
+  readonly sender: User
+
+  get getContent(): X
+
+  get getSender(): User
+}
+
+export class NotificationMessage implements Message<Notification> {
+  content: Notification
+  sender: User
+  type = 'notificationMessage'
+
+  constructor(sender: User, notification: Notification) {
+    this.content = notification
+    this.sender = sender
+  }
+
+  get getContent(): Notification {
+    return this.content
+  }
+
+  get getSender(): User {
+    return this.sender
+  }
+}
+
+export class TextMessage implements Message<string> {
+  content: string
+  sender: User
+  type = 'textMessage'
+
+  constructor(sender: User, text: string) {
+    this.content = text
+    this.sender = sender
+  }
+
+  get getContent(): string {
+    return this.content
+  }
+
+  get getSender(): User {
+    return this.sender
+  }
+}
+
+export class ChatUpdate {
+  readonly notificationMessage: NotificationMessage
+  readonly messages: TextMessage[]
+
+  constructor(notificationMessage: NotificationMessage, messages: TextMessage[]) {
+    this.notificationMessage = notificationMessage
+    this.messages = messages
+  }
+}
+
+export enum Ack {
+  OK = 0,
+  FAILURE = 1
+}
