fe63b419a2cb1f56336407c28be658f730113900
Luca Fabri <luca.fabri1999@gmail.com>
2024-10-08 18:05:59 +0200

    chore: working communication for video sync with session-service

---
+++ src/controllers/session/videoController.ts
@@ -0,0 +1,64 @@
+import {
+  MessageContent,
+  NotificationMessage,
+  TextMessage,
+  Ack
+} from '../../components/session/model/message'
+import { Message } from '../../components/session/model/message'
+import { io, Socket } from 'socket.io-client'
+import {
+  NotificationMessageDeserializer,
+  TextMessageDeserializer
+} from '../../components/session/model/presentation/deserialization/messageDeserializer'
+import { VideoState } from '../../components/session/model/video'
+import { VideoStateDeserializer } from '../../components/session/model/presentation/deserialization/videoStateDeserializer'
+import { SerializerImpl } from '../../components/session/model/presentation/serialization/messageSerializer'
+
+export interface VideoController {
+  listenToVideoEvents(
+    getVideoStateCallback: () => VideoState,
+    synchVideoCallback: (videoState: VideoState) => void
+  ): void
+  playVideo(timestamp: number): Promise<void>
+  stopVideo(timestamp: number): Promise<void>
+}
+
+export class VideoControllerImpl implements VideoController {
+  socket: Socket
+
+  constructor(socket: Socket) {
+    this.socket = socket
+  }
+
+  listenToVideoEvents(
+    getVideoStateCallback: () => VideoState,
+    synchVideoCallback: (videoState: VideoState) => void
+  ): void {
+    // Get synchronization messages
+    this.socket.on('synchronize', (data) => {
+      const videoState: VideoState = new VideoStateDeserializer().deserialize(JSON.parse(data))
+      synchVideoCallback(videoState)
+    })
+
+    // Send video state, at request
+    this.socket.on('videoState', (callback) => {
+      callback(new SerializerImpl().serialize(getVideoStateCallback()))
+    })
+  }
+
+  playVideo(timestamp: number): Promise<void> {
+    return new Promise((resolve) => {
+      this.socket.emit('playVideo', { timestamp: timestamp }, () => {
+        resolve()
+      })
+    })
+  }
+
+  stopVideo(timestamp: number): Promise<void> {
+    return new Promise((resolve) => {
+      this.socket.emit('stopVideo', { timestamp: timestamp }, () => {
+        resolve()
+      })
+    })
+  }
+}
