18777eb77dff4f0722b2471655820f302b7bbbba
Luca Fabri <luca.fabri1999@gmail.com>
2024-10-10 18:34:49 +0200

    feat: join existing room popup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,31 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import Popup from '../components/session/Popup.vue'
+
+const joinSessionModal = ref(null)
+
+function showPopup() {
+  if (joinSessionModal.value) {
+    joinSessionModal.value.show()
+  }
+}
+
+function closePopup() {
+  if (joinSessionModal.value) {
+    joinSessionModal.value.hide()
+  }
+}
+
+onMounted(() => {
+  joinSessionModal.value = new bootstrap.Modal('#joinSessionPopup')
+  joinSessionModal.value.hide()
+})
+</script>
 
 <template>
+  <Popup @closeJoinSessionPopup="closePopup"></Popup>
+  <!-- <div id="myModal"></div> -->
+
   <div class="cover-container d-flex p-3 mx-auto flex-column">
     <main role="main" class="inner cover">
       <h1 class="cover-heading">Welcome to Let's Stream It!</h1>
@@ -13,7 +38,9 @@
             <a href="#" class="btn btn-lg btn-secondary" style="width: 100%">Create a Session</a>
           </div>
           <div class="col-6">
-            <a href="#" class="btn btn-lg btn-secondary" style="width: 100%">Join a Session</a>
+            <a href="#" class="btn btn-lg btn-secondary" style="width: 100%" @click="showPopup"
+              >Join a Session</a
+            >
           </div>
         </div>
       </div>
