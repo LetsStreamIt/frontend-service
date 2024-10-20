0da2666407af5bcd2c91cf8b4626eb00039ffdbf
Luca Fabri <luca.fabri1999@gmail.com>
2024-10-21 00:44:40 +0200

    chore: about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row d-flex p-0 mx-0 align-items-center h-100">
+    <div class="container-fluid">
+      <h2 class="text-center">Let's Stream It - Members</h2>
+
+      <div class="row my-5 justify-content-center">
+        <div class="col-md-2 col-2">
+          <img src="../assets/luca_fabri.jpg" class="img-fluid" />
+          <p class="mt-2 text-center">Luca Fabri</p>
+        </div>
+        <div class="col-md-2 col-2">
+          <img src="../assets/simone_ceredi.jpg" class="img-fluid" />
+          <p class="mt-2 text-center">Simone Ceredi</p>
+        </div>
+        <div class="col-md-10 col-10">
+          <p class="mt-2">
+            Hello folks! This is Luca and Simone, two university students from the University of
+            Bologna (Unibo), Italy. Currently, we're both attending the second year of the Master's
+            degree in Computer Science and Engineering. This project, called Let's Stream It, is
+            submitted for both Software Process Engineering and Web applications and services exams.
+            The project is open source and hosted on Github
+            <a href="https://github.com/LetsStreamIt">Let's Stream It</a>. <br />
+
+            For any issues or information about the website, please feel free to contact us at our
+            university emails:
+            <a href="mailto:luca.fabri@studio.unibo.it">luca.fabri1999@gmail.com</a>,
+            <a href="mailto:simone.ceredi@studio.unibo.it">simone.ceredi@studio.unibo.it</a>.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
