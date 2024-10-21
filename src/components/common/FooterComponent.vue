2c7b8a4898d688774dbc5af76e97b710a39dd97b
Luca Fabri <luca.fabri1999@gmail.com>
2024-10-21 10:43:08 +0200

    chore: refine footer, add license page

--- src/components/common/FooterComponent.vue
+++ src/components/common/FooterComponent.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
   <footer class="text-secondary mt-auto row text-center justify-content-center p-3">
     <div class="inner">
-      <p>Developed by LetsStreamIt team. MIT licenced.</p>
+      <p>
+        Developed by LetsStreamIt team. Code licensed under the
+        <RouterLink to="/license">MIT License. </RouterLink>
+      </p>
     </div>
   </footer>
 </template>
