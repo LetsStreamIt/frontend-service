ffa70bf06213bf218b1292364e612e9b609eb889
Luca Fabri <luca.fabri1999@gmail.com>
2024-10-23 12:21:49 +0200

    feat: align text message right if coming from the local user, left otherwise

--- src/controllers/session/chatController.ts
+++ src/controllers/session/chatController.ts
@@ -1,7 +1,7 @@
 import { Socket } from 'socket.io-client'
 import { SendMessageResponse } from '@/model/command/response'
 import { Message, MessageContent, TextMessage, NotificationMessage } from '@/model/message'
-import { User } from '../../model/user'
+import { User, UserId } from '../../model/user'
 
 export interface ChatController {
   handleChatMessages(recvMessageCallback: (message: Message<MessageContent>) => void): Promise<void>
@@ -32,7 +32,7 @@ export class ChatControllerImpl implements ChatController {
   ): Promise<void> {
     this.socket.on('textMessage', (chatMessages) => {
       chatMessages.forEach((message) => {
-        const sender: User = new User(message.sender.id, message.sender.value)
+        const sender: User = new User(new UserId(message.sender.id.email), message.sender.value)
         const textMessage: TextMessage = new TextMessage(sender, message.content)
         recvMessageCallback(textMessage)
       })
