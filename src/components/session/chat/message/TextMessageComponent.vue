0567b6523689fd36d85527f96698862e2190a20d
Luca Fabri <luca.fabri1999@gmail.com>
2024-10-23 12:21:49 +0200

    feat: align text message right if coming from the local user, left otherwise

--- src/components/session/chat/message/TextMessageComponent.vue
+++ src/components/session/chat/message/TextMessageComponent.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { TextMessage } from '@/model/message'
+import { useAuthStore } from '../../../../stores/auth'
+
+const authStore = useAuthStore()
 
 const props = defineProps({
   message: {
@@ -10,10 +13,15 @@ const props = defineProps({
 })
 
 const { message } = toRefs(props)
+
+const contentAlignment: string =
+  authStore.email == message.value.getSender.getId.getEmail
+    ? ' justify-content-end'
+    : ' justify-content-start'
 </script>
 
 <template>
-  <div class="d-flex flex-row justify-content-start m-1">
+  <div class="d-flex flex-row justify-content-end m-1" :class="contentAlignment">
     <div class="p-2" style="border-radius: 15px; background-color: rgba(57, 192, 237, 0.2)">
       <p class="small mb-0">
         {{ message.getSender.value }}:
