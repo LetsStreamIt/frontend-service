2736206b1e485bb1d30c3250561ff5403978a4a5
Luca Fabri <luca.fabri1999@gmail.com>
2024-12-20 12:06:58 +0100

    chore: add robots txt

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
+import Sitemap from 'vite-plugin-sitemap'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Sitemap()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
